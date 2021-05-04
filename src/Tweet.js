f0fc13e3d47eb7c30b0abc8c4256438502cebaff
Gurkirat Sidhu <gurkiratsidhu@Gurkirats-MacBook-Pro.local>
2021-05-04 23:42:29 +0530

    second commit

---
+++ src/Tweet.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Tweet(){
+    return(
+        <div>
+            <h3> Name</h3> 
+            <p> This is a tweet</p>
+        </div>
+    )
+}
+
+export default Tweet;
